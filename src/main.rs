dcdfa11e2d7619e0ce5056427df11bf899916028
Brymer Meneses <68867487+brymer-meneses@users.noreply.github.com>
2023-05-29 10:11:50 +0800

    finish chapter 3

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 #![no_std] // avoid linking to std
 #![no_main] // remove dependency to rust runtime
 
+mod vga_buffer;
+
 use core::panic::PanicInfo;
 
 /// this function is called on panic
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
@@ -15,17 +18,8 @@ fn panic(_info: &PanicInfo) -> ! {
 /// of this function to some cryptic type.
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    static HELLO: &[u8] = b"Hello World!";
-    let vga_buffer = 0xb8000 as *mut u8;
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            // write the string byte per byte
-            *vga_buffer.offset(i as isize * 2) = byte;
-            // set color byte to `0xb` which colors it as cyan
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    println!("Hello World");
 
     loop {}
 }
